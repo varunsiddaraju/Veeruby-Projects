d81b610d819990670eedabef4077396963533792
Arunkumar70 <58801830+Arunkumar70@users.noreply.github.com>
2020-04-27 22:10:32 +0530

    ARR Connection established

---
+++ Azure Remote Rendering/Assets/Scenes/Script/RemoteRendering.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.Azure.RemoteRendering;
+using Microsoft.Azure.RemoteRendering.Unity;
+
+#if UNITY_WSA
+    using UnityEngine.XR.WSA;
+#endif
+
+// ask Unity to automatically append an ARRServiceUnity component when a RemoteRendering script is attached
+[RequireComponent(typeof(ARRServiceUnity))]
+public class RemoteRendering : MonoBehaviour
+{
+    // fill out the variables below with your account details
+
+    // AccountDomain must be '<region>.mixedreality.azure.com' - if no '<region>' is specified, connections will fail
+    // See the documentation for the list of available regions.
+    public string AccountDomain = "westus2.mixedreality.azure.com";
+    public string AccountId = "<enter your account id here>";
+    public string AccountKey = "<enter your account key here>";
+
+    public uint MaxLeaseTimeHours = 0;
+    public uint MaxLeaseTimeMinutes = 10;
+    public RenderingSessionVmSize VMSize = RenderingSessionVmSize.Standard;
+
+    private ARRServiceUnity arrService = null;
+
+    private void Awake()
+    {
+        // initialize Azure Remote Rendering for use in Unity:
+        // it needs to know which camera is used for rendering the scene
+        RemoteUnityClientInit clientInit = new RemoteUnityClientInit(Camera.main);
+        RemoteManagerUnity.InitializeManager(clientInit);
+
+        // lookup the ARRServiceUnity component and subscribe to session events
+        arrService = GetComponent<ARRServiceUnity>();
+        arrService.OnSessionErrorEncountered += ARRService_OnSessionErrorEncountered;
+        arrService.OnSessionStatusChanged += ARRService_OnSessionStatusChanged;
+    }
+
+#if !UNITY_EDITOR
+    private void Start()
+    {
+        StartCoroutine(AutoStartSessionAsync());
+    }
+#endif
+
+    private void OnDestroy()
+    {
+        arrService.OnSessionErrorEncountered -= ARRService_OnSessionErrorEncountered;
+        arrService.OnSessionStatusChanged -= ARRService_OnSessionStatusChanged;
+
+        RemoteManagerStatic.ShutdownRemoteRendering();
+    }
+
+    private void CreateFrontend()
+    {
+        if (arrService.Frontend != null)
+        {
+            // early out if the front-end has been created before
+            return;
+        }
+
+        // initialize the ARR service with our account details
+        AzureFrontendAccountInfo accountInfo = new AzureFrontendAccountInfo();
+        accountInfo.AccountKey = AccountKey;
+        accountInfo.AccountId = AccountId;
+        accountInfo.AccountDomain = AccountDomain;
+
+        arrService.Initialize(accountInfo);
+    }
+
+    public void CreateSession()
+    {
+        CreateFrontend();
+
+        // StartSession will call ARRService_OnSessionStarted once the session becomes available
+        arrService.StartSession(new RenderingSessionCreationParams(VMSize, MaxLeaseTimeHours, MaxLeaseTimeMinutes));
+    }
+
+    public void StopSession()
+    {
+        arrService.StopSession();
+    }
+
+    private void ARRService_OnSessionStatusChanged(ARRServiceUnity caller, AzureSession session)
+    {
+        LogSessionStatus(session);
+    }
+
+    private void ARRService_OnSessionErrorEncountered(ARRServiceUnity caller, SessionGeneralContext context)
+    {
+        Debug.LogError($"Session error encountered. Context: {context.ErrorMessage}. Request Cv: {context.RequestCorrelationVector}. Response Cv: {context.ResponseCorrelationVector}");
+    }
+
+    private async void LogSessionStatus(AzureSession session)
+    {
+        if (session != null)
+        {
+            var sessionProperties = await session.GetPropertiesAsync().AsTask();
+            LogSessionStatus(sessionProperties);
+        }
+        else
+        {
+            var sessionProperties = arrService.LastProperties;
+            Debug.Log($"Session ended: Id={sessionProperties.Id}");
+        }
+    }
+
+    private void LogSessionStatus(RenderingSessionProperties sessionProperties)
+    {
+        Debug.Log($"Session '{sessionProperties.Id}' is {sessionProperties.Status}. Size={sessionProperties.Size}" +
+            (!string.IsNullOrEmpty(sessionProperties.Hostname) ? $", Hostname='{sessionProperties.Hostname}'" : "") +
+            (!string.IsNullOrEmpty(sessionProperties.Message) ? $", Message='{sessionProperties.Message}'" : ""));
+    }
+
+#if UNITY_EDITOR
+    private void OnGUI()
+    {
+        if (arrService.CurrentActiveSession == null)
+        {
+            if (GUI.Button(new Rect(10, 10, 175, 30), "Create Session"))
+            {
+                CreateSession();
+            }
+        }
+        else
+        {
+            if (GUI.Button(new Rect(10, 10, 175, 30), "Stop Session"))
+            {
+                StopSession();
+            }
+        }
+    }
+#else
+    public IEnumerator AutoStartSessionAsync()
+    {
+        CreateSession();
+    }
+#endif
+}
